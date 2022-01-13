69147b0e122a8bc0a98df0b4a471182e76109ba3
Fanny <oeilden8@gmail.com>
2022-01-13 10:35:59 +0100

    début page login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './Login.css';
 
 function Login() {
-  return <div className="Login"> Login oui </div>;
+  return (
+    <div className="login">
+      <h2>IDENTIFIEZ-VOUS</h2>
+      <section className="login-container">
+        <form className="login-form">
+          <label htmlFor="mail">
+            <input type="text" placeholder="mail" />
+          </label>
+          <label htmlFor="password">
+            <input type="text" placeholder="mot de passe" />
+          </label>
+        </form>
+        <button type="submit" className="button-login">
+          LOG IN
+        </button>
+      </section>
+    </div>
+  );
 }
 
 export default Login;
