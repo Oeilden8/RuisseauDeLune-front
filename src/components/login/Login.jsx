7f85cd4eb57f079510f31c4f89f34eaf1f8305c8
Fanny <oeilden8@gmail.com>
2022-01-13 11:04:04 +0100

    formulaire fini et responsive

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,10 +8,10 @@ function Login() {
       <section className="login-container">
         <form className="login-form">
           <label htmlFor="mail">
-            <input type="text" placeholder="mail" />
+            <input type="text" placeholder="MAIL" />
           </label>
           <label htmlFor="password">
-            <input type="text" placeholder="mot de passe" />
+            <input type="text" placeholder="MOT DE PASSE" />
           </label>
         </form>
         <button type="submit" className="button-login">
